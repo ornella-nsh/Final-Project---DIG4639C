db171c797a5ad81c04bb664649b51752b6564d87
ornella-nsh <152661131+ornella-nsh@users.noreply.github.com>
2025-04-21 15:06:32 -0400

    animation implemented, but error using motion

--- my-portfolio/src/pages/Home.jsx
+++ my-portfolio/src/pages/Home.jsx
@@ -1,15 +1,18 @@
 import styles from "./Home.module.css";
+import FadeInWrapper from "../components/FadeInWrapper";
 
 export default function Home() {
   return (
-    <section className={styles.home}>
-      <h1>Hi there, my name is Ornella!</h1>
-      <p>Welcome to my portfolio.</p>
-      <img
-        src="/assets/homepageimage.jpg"
-        alt="... description ..."
-        className={styles.homeImage}
-      />
-    </section>
+    <FadeInWrapper>
+      <section className={styles.home}>
+        <h1>Hi there, my name is Ornella!</h1>
+        <p>Welcome to my portfolio.</p>
+        <img
+          src="/assets/homepageimage.jpg"
+          alt="... description ..."
+          className={styles.homeImage}
+        />
+      </section>
+    </FadeInWrapper>
   );
 }
