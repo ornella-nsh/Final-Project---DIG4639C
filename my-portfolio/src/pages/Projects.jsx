691d1e2aa2087e1ef366e2b0d29b90cb1ff04ad4
ornella-nsh <152661131+ornella-nsh@users.noreply.github.com>
2025-04-21 19:27:04 -0400

    added styling & debugged

--- my-portfolio/src/pages/Projects.jsx
+++ my-portfolio/src/pages/Projects.jsx
@@ -5,27 +5,27 @@ const projects = [
     title: "Shelfmate",
     description: "A pantry management solution with wellness in mind.",
     image: "/src/assets/project1.png",
-    link: "https://github.com/your/project1",
+    link: "https://ornella-nsh.framer.website/shelfmate",
   },
 
   {
     title: "Marketing & Graphic Design",
     description: "A collection of recent graphic design projects.",
     image: "/src/assets/project2.jpg",
-    link: "",
+    link: "https://ornella-nsh.framer.website/graphic-design",
   },
   {
     title: "Amazon Redesign",
     description:
       "Amazon homepage and navigation redesign for a school project, made in Figma.",
     image: "src/assets/project3.png",
-    link: "",
+    link: "https://ornella-nsh.framer.website/amazon-redesign",
   },
   {
     title: "ARound the World",
     description: "Augmented Reality app concept & product pitch.",
     image: "src/assets/project4.png",
-    link: "",
+    link: "https://ornella-nsh.framer.website/around-the-world",
   },
 ];
 
@@ -37,7 +37,7 @@ export default function Projects() {
         {projects.map((proj) => (
           <div key={proj.title}>
             <h3>{proj.title}</h3>
-            <img src={proj.image} alt={proj.title} />
+            <img src={proj.image} alt={proj.title + " Cover Image"} />
             <p>{proj.description}</p>
             <a href={proj.link} target="_blank" rel="noopener noreferrer">
               View Project
